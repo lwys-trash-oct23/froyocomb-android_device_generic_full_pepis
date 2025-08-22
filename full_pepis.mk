061cf9c3554523ee6afa431798e72629c5f628f2
Liu Wenyuan <dobby@ubuntu12-android-vm.d2lst.ml>
2025-08-22 15:47:15 +0800

    Update according to new AOSP docs

--- full_pepis.mk
+++ full_pepis.mk
@@ -1,14 +1,15 @@
-# Add live wallpapers
-PRODUCT_PACKAGES := \
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
+
+PRODUCT_NAME := full_pepis
+PRODUCT_DEVICE := generic
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Full Android PEPIS EDITION on Emulator
+PRODUCT_MANUFACTURER := Goggle
+
+# Add live wallpaper-related apps
+PRODUCT_PACKAGES += \
     LivePicker \
     Basic \
     HoloSpiral \
     MagicSmoke \
     MusicVisualization
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
-
-PRODUCT_NAME := full_pepis
-PRODUCT_DEVICE := generic
-PRODUCT_BRAND := NOT Android
-PRODUCT_MODEL := Full Android PEPIS EDITION on Emulator
